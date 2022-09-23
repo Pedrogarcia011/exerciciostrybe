fa3475c6f064f9b2b9e7d583ef2c6143c9786553
Pedrogarcia011 <pedrohenriquegracia011@gmail.com>
2022-09-23 17:53:10 -0300

    exercicio 2 Sala 7

---
+++ Fundamentos_Web/Bloco4/Dia.03/Aula.js
@@ -0,0 +1,14 @@
+let inicial = 2
+let final = 150
+let contador = 0
+
+for(let index=inicial; index <=final; index++ ){
+    if (index % 3 == 0){
+        contador++
+    }
+    console.log (contador)
+} if (contador = 50){
+    console.log ('Que legal exixtem 50 numeros divisiveis por 3')
+}else {
+    console.log ('existem mais de 50 numeros divisiveis por 3')
+}
